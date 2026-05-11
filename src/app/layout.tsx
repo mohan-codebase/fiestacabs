d0e55224a68ebb83f80d7f70120ebaf2b7f31df4
Mohan <173985403+mohan-codebase@users.noreply.github.com>
2026-05-11 12:32:16 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,16 +71,20 @@ export default function RootLayout({
           <ScrollToTop />
           <GlobalModal />
         </ModalProvider>
-        <script
+        <Script id="zsiqchat" strategy="afterInteractive">
+          {`
+            var $zoho=$zoho || {};
+            $zoho.salesiq = $zoho.salesiq || {
+              widgetcode: "siq8dc889cb6780acd2bfd30fc91d07dcdb44bae525f81bfa66e5d65f1c2414880e016070d80d383b405e205f9433dd516b", 
+              values: {},
+              ready: function() {}
+            };
+          `}
+        </Script>
+        <Script
           id="zsiqscript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.$zoho=window.$zoho || {};$zoho.salesiq=$zoho.salesiq||{ready:function(){}};
-              var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;
-              s.src="https://salesiq.zohopublic.in/widget?wc=siq8dc889cb6780acd2bfd30fc91d07dcdb44bae525f81bfa66e5d65f1c2414880e016070d80d383b405e205f9433dd516b";
-              t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);
-            `,
-          }}
+          src="https://salesiq.zoho.in/widget?wc=siq8dc889cb6780acd2bfd30fc91d07dcdb44bae525f81bfa66e5d65f1c2414880e016070d80d383b405e205f9433dd516b"
+          strategy="afterInteractive"
         />
 
       </body>
